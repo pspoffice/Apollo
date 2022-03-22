0dde3d513e6f306eb84e3a3a6e1c8340af4fde47
Priyank S Patel <pspoffice365@gmail.com>
2022-03-22 09:25:56 +0530

    1647 A

---
+++ CF/1647/A.cpp
@@ -0,0 +1,36 @@
+// https://codeforces.com/contest/1647/problem/A
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+#define FAST ios::sync_with_stdio(false), cin.tie(nullptr)
+
+auto Solve() -> void {
+  int n;
+  cin >> n;
+  int x;
+  if (n % 3 == 1) {
+    x = 1;
+  } else {
+    x = 2;
+  }
+  int s = 0;
+  while (s != n) {
+    cout << x;
+    s += x;
+    x = 3 - x;
+  }
+  cout << '\n';
+}
+
+auto main() -> int {
+  FAST;
+  int t;
+  cin >> t;
+  while (t--) {
+    Solve();
+  }
+  return 0;
+}
