c3e8c90535e98907a2867addec8da0cacff79b29
Priyank S Patel <pspoffice365@gmail.com>
2022-04-01 23:05:53 +0530

    245 D

---
+++ AC/ABC/245/D.cpp
@@ -0,0 +1,38 @@
+// https://atcoder.jp/contests/abc245/tasks/abc245_c
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#define FAST ios::sync_with_stdio(false), cin.tie(nullptr)
+
+auto Solve() -> void {
+  int n, m;
+  cin >> n >> m;
+  vector<int> a(n + 1);
+  vector<int> b(m + 1);
+  vector<int> c(n + m + 1);
+  for (int i = 0; i <= n; i++) {
+    cin >> a[i];
+  }
+  for (int i = 0; i <= n + m; i++) {
+    cin >> c[i];
+  }
+  for (int i = m; i >= 0; i--) {
+    b[i] = c[i + n] / a[n];
+    for (int j = 0; j <= n; j++) {
+      c[i + j] -= b[i] * a[j];
+    }
+  }
+  for (int i = 0; i < m; i++) {
+    cout << b[i] << ' ';
+  }
+  cout << b[m] << '\n';
+}
+
+auto main() -> int {
+  FAST;
+  Solve();
+  return 0;
+}
