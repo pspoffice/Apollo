56a34a158eb7bcda7206233ee9e8bdf43e9491f1
Priyank S Patel <pspoffice365@gmail.com>
2022-05-21 18:19:49 +0530

    252 A

---
+++ AC/ABC/252/A.cpp
@@ -0,0 +1,18 @@
+// https://atcoder.jp/contests/abc252/tasks/abc252_a
+
+#include <iostream>
+using namespace std;
+
+#define FAST ios::sync_with_stdio(false), cin.tie(nullptr)
+
+auto Solve() -> void {
+  int n;
+  cin >> n;
+  cout << char(n) << '\n';
+}
+
+auto main() -> int {
+  FAST;
+  Solve();
+  return 0;
+}
