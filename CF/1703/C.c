fd1347705f0353fb25f4373811b24a987b91f0e7
Priyank Patel <priyank@pop-os.localdomain>
2022-07-21 12:24:35 +0530

    1703 C

--- CF/1703/C.c
+++ CF/1703/C.c
@@ -4,38 +4,47 @@
 #include <stdlib.h>
 #include <string.h>
 
+void Output(const int n, const int *arr) {
+  for (int i = 0; i < n; ++i) {
+    printf("%d ", arr[i]);
+  }
+  putchar('\n');
+}
+
+void Process(const int n, int *arr) {
+  for (int i = 0; i < n; ++i) {
+    int size;
+    scanf("%d ", &size);
+    char str[15];
+    fgets(str, size + 1, stdin);
+    for (int j = 0; j < size; ++j) {
+      if (str[j] == 'U') {
+        arr[i] -= 1;
+      } else {
+        arr[i] += 1;
+      }
+      if (arr[i] < 0) {
+        arr[i] += 10;
+      }
+      arr[i] %= 10;
+    }
+  }
+}
+
 void Solve() {
-  // Input
+  // Store Input
   int n;
   scanf("%d ", &n);
   int *a = (int *)malloc((unsigned long)n * sizeof(int));
   for (int i = 0; i < n; ++i) {
     scanf("%d", a + i);
   }
-  // Process
-  for (int i = 0; i < n; ++i) {
-    int x;
-    scanf("%d ", &x);
-    char s[15];
-    fgets(s, x + 1, stdin);
-    for (int j = 0; j < x; ++j) {
-      if (s[j] == 'U') {
-        a[i] -= 1;
-      } else {
-        a[i] += 1;
-      }
-      if (a[i] < 0) {
-        a[i] += 10;
-      }
-      a[i] %= 10;
-    }
-  }
-  // Output
-  for (int i = 0; i < n; ++i) {
-    printf("%d ", a[i]);
-  }
+  // Processing Data
+  Process(n, a);
+  // Print Output
+  Output(n, a);
+  // Clean
   free(a);
-  putchar('\n');
 }
 
 int main() {
